0f1e1f7873f32479be469edc638383a96c4a7227
0w0 <55968557+rewrite0w0@users.noreply.github.com>
2025-05-22 00:15:56 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -20,7 +20,7 @@ require('lazy').setup({  -- lazy.nvim의 플러그인 목록과 설정을 정의
 		lazy = false,
 		priority = 1000,
 		config = function()
-		  vim.cmd([[colorscheme tokyonight]])
+		  vim.cmd([[colorscheme tokyonight-day]])
 		end,
 	
 	},
@@ -29,7 +29,8 @@ require('lazy').setup({  -- lazy.nvim의 플러그인 목록과 설정을 정의
     -- Rust 언어 지원
     'rust-lang/rust.vim',  -- Rust 파일에 대한 구문 강조 및 포맷팅 지원
     -- 코드 포맷팅: vim-prettier
-    { 'prettier/vim-prettier', ft = {'javascript', 'typescript', 'css', 'json', 'markdown', 'vue', 'svelte', 'yaml', 'html', 'javascriptreact', 'typescriptreact'} },  -- Prettier 플러그인
+    -- { 'prettier/vim-prettier', ft = {'javascript', 'typescript', 'css', 'json', 'markdown', 'vue', 'svelte', 'yaml', 'html', 'javascriptreact', 'typescriptreact'} },  -- Prettier 플러그인
+	{ 'prettier/vim-prettier', ft = {'javascript', 'typescript', 'css', 'json', 'markdown', 'vue', 'svelte', 'yaml', 'html', 'javascriptreact', 'typescriptreact'}, commit = 'latest' },
     -- Lua 유틸리티: plenary.nvim
     'nvim-lua/plenary.nvim',  -- telescope.nvim 등 Lua 플러그인의 공통 유틸리티
     -- 파일/텍스트 검색: telescope.nvim
@@ -80,7 +81,7 @@ vim.g.loaded_ruby_provider = 0  -- Ruby provider 비활성화
 vim.g.loaded_perl_provider = 0  -- Perl provider 비활성화
 
 -- 전역 변수: 플러그인별 설정
-vim.g.coc_global_extensions = { 'coc-json', 'coc-prettier', 'coc-tsserver' }  -- coc.nvim 확장 목록
+-- vim.g.coc_global_extensions = { 'coc-json', 'coc-prettier', 'coc-tsserver' }  -- coc.nvim 확장 목록
 vim.g.prettier_autoformat = 1  -- Prettier로 자동 포맷팅 활성화
 vim.g['airline#extensions#tabline#enabled'] = 1  -- vim-airline의 탭라인 기능 활성화
 vim.g['airline#extensions#tabline#fnamemod'] = ':t'  -- 탭라인에 파일 이름만 표시
@@ -102,6 +103,10 @@ vim.opt.laststatus = 2  -- 항상 상태바 표시
 vim.opt.splitright = true  -- 새 창을 오른쪽에 분할
 vim.opt.splitbelow = true  -- 새 창을 아래쪽에 분할
 vim.opt.updatetime = 250  -- 커서 업데이트 주기
+
+vim.opt.shell = 'powershell'  -- PowerShell을 기본 셸로 설정
+vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command'
+vim.opt.shellxquote = ''
 
 -- 키 매핑: 사용자 정의 단축키 설정
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')  -- <Leader>ff: Telescope로 파일 검색
@@ -135,12 +140,17 @@ require('toggleterm').setup({  -- toggleterm 플러그인 설정
     size = 10,  -- 터미널 창 크기
     open_mapping = [[<C-t>]],  -- <C-t>로 터미널 열기/닫기 (<C-n> 충돌 방지)
     direction = 'horizontal',  -- 터미널을 수평으로 열기
+	shade_terminals = true,  -- 터미널 배경을 어둡게 처리 (시각적 구분)
+	persist_mode = true,  -- 터미널 모드 유지로 키 입력 처리 안정화
 })
 
+-- vim.keymap.set('t', '<C-c>', '<C-\\><C-c>', { noremap = true, silent = true })
+
+vim.g['prettier#exec_cmd_path'] = '경로/prettier.cmd' -- 경로 지정, 하드코딩이므로 나중에 변경할 것
 -- 자동 명령: 특정 조건에서 자동 실행
 vim.api.nvim_create_autocmd('BufWritePre', {  -- 파일 저장 전 자동 명령
-    pattern = { '*.js', '*.jsx', '*.json', '*.css', '*.md', '*.ts', '*.tsx' },  -- 지정된 파일 형식에 적용
-    command = 'PrettierAsync',  -- 저장 전 Prettier로 비동기 포맷팅
+    pattern = { '*.js', '*.jsx', '*.json', '*.css', '*.md', '*.ts', '*.tsx', '*.html' },  -- 지정된 파일 형식에 적용
+    command = 'Prettier',  -- 저장 전 Prettier로 비동기 포맷팅
 })
 
 -- 하이라이트: vim-gitgutter의 Git 변경 표시 색상 설정
