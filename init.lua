9221179d99e0a53401141de597aee4d9ab8c4e1b
rewrite0w0 <rewrite0w0@noreply.codeberg.org>
2026-06-04 06:49:13 +0200

    chore: 방향키 제한

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- ==========================================================================
--- 1. 기본 옵션 (Windows 11 PowerShell & 기본 UI)
+-- 1. 기본 옵션 (플러그인 로드 전에 설정하는 것이 좋습니다)
 -- ==========================================================================
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- 윈도우 11 PowerShell 설정
+-- 윈도우 11 PowerShell 설정 (필수: 속도 및 호환성)
 vim.opt.shell = "powershell.exe"
 vim.opt.shellcmdflag = "-NoProfile -NoLogo -ExecutionPolicy RemoteSigned -Command"
 vim.opt.shellquote = ""
@@ -13,24 +13,32 @@ vim.opt.shellpipe = "| Out-File -Encoding UTF8"
 vim.opt.shellredir = "| Out-File -Encoding UTF8"
 
 -- UI 및 동작 설정
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.termguicolors = true   -- 트루컬러 지원
+vim.opt.number = true          -- 줄 번호
+vim.opt.relativenumber = true  -- 상대 줄 번호 (이동 시 편함)
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 250
-vim.opt.clipboard = "unnamedplus"
+vim.opt.expandtab = true       -- 탭을 스페이스로 변환
+vim.opt.wrap = true           -- 줄 바꿈 안 함 (취향따라 true로 변경)
+vim.opt.linebreak = true  -- 단어 단위로 줄바꿈
+vim.opt.scrolloff = 8          -- 커서 위아래 여백 확보
+vim.opt.signcolumn = "yes"     -- 깃 사인 등이 올 때 흔들림 방지
+vim.opt.updatetime = 250       -- 반응 속도
+vim.opt.clipboard = "unnamedplus" -- 윈도우 클립보드와 연동 (Ctrl+V 가능)
 
--- 줄 바꿈 설정
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.opt.breakindent = true
-vim.opt.showbreak = "↳ "
+
+-- 일반, 입력, 비주얼 모드 전체에서 방향키 차단
+local modes = { 'n', 'i', 'v' }
+local arrows = { '<Up>', '<Down>', '<Left>', '<Right>' }
+
+for _, mode in ipairs(modes) do
+    for _, arrow in ipairs(arrows) do
+        vim.keymap.set(mode, arrow, '<Nop>', { noremap = true, silent = true })
+    end
+end
+
 
 -- ==========================================================================
 -- 2. Lazy.nvim 부트스트래핑
@@ -42,14 +50,15 @@ if not vim.loop.fs_stat(lazypath) then
         "https://github.com/folke/lazy.nvim.git", "--branch=stable",
         lazypath,
     })
+    print("Installing lazy.nvim...")
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- ==========================================================================
--- 3. 플러그인 목록 (담백한 핵심 구성)
+-- 3. 플러그인 목록
 -- ==========================================================================
 require("lazy").setup({
-    -- [테마]
+    -- [테마] TokyoNight
     {
         "folke/tokyonight.nvim",
         lazy = false,
@@ -59,13 +68,13 @@ require("lazy").setup({
         end,
     },
 
-    -- [탐색] Telescope
+    -- [탐색] Telescope + UI Select (추천)
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.8",
         dependencies = { 
             "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope-ui-select.nvim"
+            "nvim-telescope/telescope-ui-select.nvim" -- 코드 액션 등을 예쁘게
         },
         config = function()
             local telescope = require("telescope")
@@ -83,18 +92,21 @@ require("lazy").setup({
         "nvim-treesitter/nvim-treesitter", 
         build = ":TSUpdate", 
         event = { "BufReadPre", "BufNewFile" },
-        config = function()   
+        config = function()
+            -- [중요] 컴파일러 강제 설정 (Windows 오류 해결 핵심)
+            require("nvim-treesitter.install").compilers = { "zig" }
+    
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { 
-                    "rust", "lua", "javascript", "typescript", "tsx", "html", "css", "json"
-                },
+                ensure_installed = { "python", "rust", "lua", "javascript", "typescript", "tsx", "json", "html", "css", "bash", "markdown", "markdown_inline" },
                 highlight = { enable = true },
                 indent = { enable = true },
             })
         end,
     },
+    
 
-    -- [파일 탐색기]
+   
+    -- [파일 탐색기] Oil.nvim (강력 추천)
     { 
         "stevearc/oil.nvim", 
         config = true, 
@@ -102,19 +114,58 @@ require("lazy").setup({
         keys = { { "-", "<cmd>Oil<cr>", desc = "Open parent directory" } } 
     },
 
-    -- [Git 표시]
+    -- [Git] GitSigns + Neogit
     { "lewis6991/gitsigns.nvim", event = "BufReadPre", config = true },
+    { 
+        "NeogitOrg/neogit", 
+        dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" }, 
+        cmd = "Neogit",
+        config = true
+    },
 
-    -- [상태바]
+    -- [상태바] Lualine
     { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, config = true },
 
-    -- [코딩 편의성]
+    -- [편의성] Auto Pairs, Surround, Comment
     { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
     { "echasnovski/mini.surround", config = true },
     { "numToStr/Comment.nvim", config = true },
+    { "echasnovski/mini.indentscope", config = true, event = "BufReadPre" }, -- 들여쓰기 가이드
+
+    -- [편의성] Todo Comments (추천!) - TODO, FIXME 등을 하이라이팅
+    { 
+        "folke/todo-comments.nvim", 
+        dependencies = { "nvim-lua/plenary.nvim" }, 
+        event = "BufReadPre",
+        config = true 
+    },
+
+    -- [가이드] Which Key
     { "folke/which-key.nvim", config = true },
 
-    -- [자동완성 엔진]
+    -- [이동] Flash
+    { 
+        "folke/flash.nvim", 
+        event = "VeryLazy", 
+        opts = {}, 
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        },
+    },
+
+    -- [에러 목록] Trouble
+    { 
+        "folke/trouble.nvim", 
+        config = true,
+        keys = {
+            { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+        },
+    },
+
+    -- [UI] No Neck Pain (중앙 정렬)
+    { "shortcuts/no-neck-pain.nvim", cmd = "NoNeckPain", config = true },
+
+    -- [자동완성] CMP
     {
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
@@ -122,6 +173,7 @@ require("lazy").setup({
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
             "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip",
         },
@@ -155,7 +207,7 @@ require("lazy").setup({
         end,
     },
 
-    -- [LSP 기본 백본]
+    -- [LSP] Config & Mason
     { 
         "neovim/nvim-lspconfig",
         dependencies = {
@@ -164,10 +216,10 @@ require("lazy").setup({
         }
     },
 
-    -- [포맷터 관리]
+    -- [Formatting] Conform
     { "stevearc/conform.nvim", event = "BufWritePre" },
 
-   -- [터미널]
+    -- [터미널] Toggleterm
     {
         "akinsho/toggleterm.nvim",
         version = "*",
@@ -176,38 +228,55 @@ require("lazy").setup({
                 size = 15,
                 open_mapping = [[<C-t>]],
                 direction = "horizontal",
+                -- 윈도우 PowerShell 설정
                 shell = "powershell.exe",
             })
         end,
     },
-		}, {
-		rocks = { enabled = false },
+    
+    -- 업데이트 확인
+    checker = { enabled = true, notify = false },
 })
 
 -- ==========================================================================
--- 4. LSP 설정 (JS, TS, Rust, HTML, CSS, JSON)
+-- 4. LSP 설정 (여기가 제일 중요합니다)
+-- ==========================================================================
+-- ==========================================================================
+-- 4. LSP 설정 (수정됨: handlers를 setup 안으로 통합)
 -- ==========================================================================
 require("mason").setup()
 
 local lspconfig = require("lspconfig")
+-- cmp-nvim-lsp가 설치되어 있다면 capabilities 가져오기, 아니면 기본값
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 if pcall(require, "cmp_nvim_lsp") then
     capabilities = require("cmp_nvim_lsp").default_capabilities()
 end
 
 require("mason-lspconfig").setup({
-    ensure_installed = { 
-        "rust_analyzer", "biome", "ts_ls", "html", "cssls", "jsonls"
-    },
+    ensure_installed = { "pyright", "ruff", "rust_analyzer", "biome", "emmet_language_server" },
     automatic_installation = true,
     
+    -- 여기서 핸들러를 정의합니다 (setup_handlers 대신 사용)
     handlers = {
+        -- 1. 기본 핸들러 (모든 서버에 공통 적용)
         function(server_name)
             lspconfig[server_name].setup({
                 capabilities = capabilities,
             })
         end,
 
+        -- 2. Ruff 설정 (Hover 끄기)
+        ["ruff"] = function()
+            lspconfig.ruff.setup({
+                capabilities = capabilities,
+                on_attach = function(client)
+                    client.server_capabilities.hoverProvider = false
+                end,
+            })
+        end,
+
+        -- 3. Rust Analyzer 설정 (Clippy)
         ["rust_analyzer"] = function()
             lspconfig.rust_analyzer.setup({
                 capabilities = capabilities,
@@ -218,27 +287,18 @@ require("mason-lspconfig").setup({
                 },
             })
         end,
-
-        ["ts_ls"] = function()
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-        end,
     }
 })
 
 -- ==========================================================================
--- 5. Formatting 설정 (Conform - 자동 포맷팅)
+-- 5. Formatting 설정 (Conform)
 -- ==========================================================================
 require("conform").setup({
     formatters_by_ft = {
+        python = { "ruff_format", "ruff_fix" },
         javascript = { "biome" },
         typescript = { "biome" },
-        javascriptreact = { "biome" },
-        typescriptreact = { "biome" },
         json = { "biome" },
-        html = { "biome" },
-        css = { "biome" },
         rust = { "rustfmt" },
     },
     format_on_save = {
@@ -252,24 +312,28 @@ require("conform").setup({
 -- ==========================================================================
 local builtin = require("telescope.builtin")
 
--- Telescope 검색
+-- Telescope
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
 
--- 창 이동 (Alt + h, j, k, l)
+-- Git
+vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
+
+-- Window Navigation (Alt + hjkl)
 vim.keymap.set("n", "<A-h>", "<C-w>h", { noremap = true })
 vim.keymap.set("n", "<A-j>", "<C-w>j", { noremap = true })
 vim.keymap.set("n", "<A-k>", "<C-w>k", { noremap = true })
 vim.keymap.set("n", "<A-l>", "<C-w>l", { noremap = true })
 
--- 터미널 모드 창 이동 및 탈출
+-- Terminal Window Navigation (터미널 모드에서 나갈 때)
 vim.keymap.set("t", "<A-h>", [[<C-\><C-n><C-w>h]], { noremap = true })
 vim.keymap.set("t", "<A-j>", [[<C-\><C-n><C-w>j]], { noremap = true })
 vim.keymap.set("t", "<A-k>", [[<C-\><C-n><C-w>k]], { noremap = true })
 vim.keymap.set("t", "<A-l>", [[<C-\><C-n><C-w>l]], { noremap = true })
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
--- Oil.nvim 파일 탐색기 연동
-vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", { desc = "File Explorer" })
+-- 기타 편의
+vim.keymap.set("n", "<leader>n", "<cmd>NoNeckPain<cr>", { desc = "Toggle NoNeckPain" })
+vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", { desc = "File Explorer" }) -- Oil 단축키 하나 더 추가
