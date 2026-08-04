905070f10d86ca10e2c17ac1950a992327b1b654
rewrite0w0 <rewrite0w0@noreply.codeberg.org>
2026-08-04 18:33:15 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -113,10 +113,20 @@ vim.opt.number = true              -- 줄 번호
 vim.opt.relativenumber = true      -- 상대 줄 번호
 vim.opt.cursorline = true          -- 현재 줄 강조
 
--- Windows에서 PowerShell 설정
+
+-- Windows PowerShell 설정 (toggleterm 포함 터미널이 제대로 열리게 함)
 if is_windows then
-    vim.opt.shell = "powershell"
-    vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+  local powershell_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
 end
 
 -- =============================================
